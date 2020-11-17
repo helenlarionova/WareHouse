e270ff22635dcce2435334f4fbcf4436ac6dd2a5
Elena <kea93@mail.ru>
2020-11-17 12:00:13 +0400

    add searchview

--- app/src/main/java/com/example/android/ecwidsclad/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/ecwidsclad/ui/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.android.ecwidsclad.ui
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.SearchView
 import com.example.android.ecwidsclad.R
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,6 +24,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.action_search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
         return true
     }
 
@@ -29,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_search -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
